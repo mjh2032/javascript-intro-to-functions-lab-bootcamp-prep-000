8d7e7fef75e8975b73b4ac4faa891441260a36d6
Maria <mjh2032@gmail.com>
2018-01-25 17:40:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+var uppercase ="HELLO!"
+uppercase.toUpperCase() === uppercase
+var lowercase = 'hello!'
+lowercase.toLowerCase() === lowercase
+var mixedCase = 'Hi there!'
+mixedCase.toLowerCase() === mixedCase
+mixedCase.toUpperCase() === mixedCase
+
 function shout (string){
   return string,toUpperCase()
 }'hello')).teEqual('HELLO')
